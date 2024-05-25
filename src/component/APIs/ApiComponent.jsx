03c29a01dbfaedba7ee4d51237dfe47a5de7bc40
Peerapat_sut <89067174+Kurigaya@users.noreply.github.com>
2024-05-25 23:31:46 +0700

    Update ApiComponent.jsx

--- src/component/APIs/ApiComponent.jsx
+++ src/component/APIs/ApiComponent.jsx
@@ -1,4 +1,4 @@
-export const baseUrl = 'http://127.0.0.1:8000';
+export const baseUrl = 'http://54.66.21.117:8000';
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
